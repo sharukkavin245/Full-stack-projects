6bc1373300b6c63deada4ec3df8b2cff6ba2e300
sharukkavin245 <sharukavin245@gmail.com>
2026-06-28 22:55:56 +0530

    starting to add udpate functionality

--- FRONTEND/ems-frontend/src/components/ListEmployeeComponent.jsx
+++ FRONTEND/ems-frontend/src/components/ListEmployeeComponent.jsx
@@ -1,52 +1,56 @@
-import React,{ useEffect,useState } from 'react'
-import { listEmployees } from '../services/EmployeeService'
-import { useNavigate } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import { listEmployees } from "../services/EmployeeService";
+import { useNavigate } from "react-router-dom";
 
 const ListEmployeeComponent = () => {
+  const [employees, setEmployees] = useState([]);
+  const navigator = useNavigate();
 
-    const [employees, setEmployees] = useState([]);
-    const navigator = useNavigate();
+  useEffect(() => {
+    listEmployees()
+      .then((response) => {
+        setEmployees(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
 
-    useEffect(() => {
-        listEmployees().then((response) => {
-            setEmployees(response.data);
-        }).catch(error => {
-            console.error(error);
-        })
-    },[]);
-
-    function addNewEmployee(){
-        navigator('/add-employee')
-    }
+  function addNewEmployee() {
+    navigator("/add-employee");
+  }
   return (
-    <div className='container'>
-
-        <h2 className='text-center'>List of Employees</h2>
-        <button className='btn btn-primary mb-2' onClick={addNewEmployee}>Add Employee</button>
-        <table className='table table-striped table-bordered'>
-            <thead>
-                <tr>
-                    <th>Employee Id</th>
-                    <th>Employee First Name</th>
-                    <th>Employee Second Name</th>
-                    <th>Employee Email Id</th>
-                </tr>
-            </thead>
-            <tbody>
-                {
-                    employees.map(employee => 
-                        <tr key={employee.id}>
-                            <td>{employee.id}</td>
-                            <td>{employee.firstName}</td>
-                            <td>{employee.lastName}</td>
-                            <td>{employee.email}</td>
-                        </tr>
-                    )
-                }
-            </tbody>
-        </table>
+    <div className="container">
+      <h2 className="text-center">List of Employees</h2>
+      <button className="btn btn-primary mb-2" onClick={addNewEmployee}>
+        Add Employee
+      </button>
+      <table className="table table-striped table-bordered">
+        <thead>
+          <tr>
+            <th>Employee Id</th>
+            <th>Employee First Name</th>
+            <th>Employee Second Name</th>
+            <th>Employee Email Id</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {employees.map((employee) => (
+            <tr key={employee.id}>
+              <td>{employee.id}</td>
+              <td>{employee.firstName}</td>
+              <td>{employee.lastName}</td>
+              <td>{employee.email}</td>
+              <td>
+                <button className='btn btn-info' onClick={() => udpateEmployee(employee.id)}>Update</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
-  )
-}
+  );
+};
 
-export default ListEmployeeComponent
+export default ListEmployeeComponent;
